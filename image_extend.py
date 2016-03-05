0faaf77f4ead1078ddf187502eb95915f8458f3b
xiaobai <dx5878210@gmail.com>
2016-03-05 17:58:05 +0800

    first commit

--- image_extend.py
+++ image_extend.py
@@ -58,19 +58,37 @@ class Appium_Extend(object):
         else:
             raise Exception("%s is not exist" %image_path)
 
+    #图片分割
+    def split_image(self, img, part_size=(64, 64)):
+        w, h = img.size
+
+        pw, ph = part_size
+
+        assert w % pw == h % ph == 0
+
+        return [img.crop((i, j, i+pw, j+ph)).copy() \
+
+                    for i in range(0, w, pw) \
+
+                    for j in range(0, h, ph)]
+
+
+    #图片直方图对比
+    def hist_similar(self, lh, rh):
+        assert len(lh) == len(rh)
+        return sum(1 - (0 if l == r else float(abs(l - r))/max(l, r)) for l, r in zip(lh, rh))/len(lh)
+
     #对比图片
     def same_as(self, load_image, percent):
-
-
         image1 = Image.open(TEMP_FILE)
         image2 = load_image
-        image1 = image1.resize((256, 256)).convert('RGB')
-        image2 = image2.resize((256, 256)).convert('RGB')
-        lh = image1.histogram()
-        rh = image2.histogram()
-        differ = sum(1 - (0 if l == r else float(abs(l - r))/max(l, r)) for l, r in zip(lh, rh))/len(lh)
+        li = image1.resize((256, 256)).convert('RGB')
+        ri = image2.resize((256, 256)).convert('RGB')
+
+        differ = sum(self.hist_similar(l.histogram(), r.histogram()) for l, r in zip(self.split_image(li), self.split_image(ri))) / 16.0
+        #differ = sum(1 - (0 if l == r else float(abs(l - r))/max(l, r)) for l, r in zip(lh, rh))/len(lh)
         # differ = math.sqrt(reduce(operator.add,  list(map(lambda a,b: (a-b)**2,  histogram1, histogram2)))/len(histogram1) )
-        if differ <= percent:
+        if differ >= percent:
             return True
         else:
             return False
