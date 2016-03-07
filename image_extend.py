55c8350eb52c3e4298e63348642999876f30e8c2
xiaobai <dx5878210@gmail.com>
2016-03-07 15:59:54 +0800

    使用google相似图片比较算法

--- image_extend.py
+++ image_extend.py
@@ -1,9 +1,8 @@
 import os
-import platform
 import tempfile
 import shutil
-import math
-import operator
+import glob
+import sys
 from functools import reduce
 
 from PIL import Image
@@ -13,26 +12,33 @@ TEMP_FILE = PATH(tempfile.gettempdir() + "/temp_screen.png")
 
 
 class Appium_Extend(object):
+
     def __init__(self, driver):
         self.driver = driver
 
-    #获取整个屏幕
+    # 获取整个屏幕
     def get_screenshot_by_element(self, element):
         self.driver.get_screenshot_as_file(TEMP_FILE)
 
-        #获取元素
+        # 获取元素
         location = element.location
         size = element.size
-        box = (location['x'], location['y'], location['x']+size['width'], location['y']+size['height'])
+        box = (
+            location['x'],
+            location['y'],
+            location['x'] +
+            size['width'],
+            location['y'] +
+            size['height'])
 
-        #获取图片
+        # 获取图片
         image = Image.open(TEMP_FILE)
         newimage = image.crop(box)
         newimage.save(TEMP_FILE)
 
         return self
 
-    #自定义截图
+    # 自定义截图
     def get_screenshot_by_custom_size(self, start_x, start_y, end_x, end_y):
         self.driver.get_screenshot_as_file(TEMP_FILE)
         box = (start_x, start_y, end_x, end_y)
@@ -43,22 +49,28 @@ class Appium_Extend(object):
 
         return self
 
-
-    #将截屏文件复制到指定目录
-    def write_to_file(self, dirpath, imagename,form = 'png'):
+    # 将截屏文件复制到指定目录
+    def write_to_file(self, dirpath, imagename, form='png'):
         if not os.path.isdir(dirpath):
             os.makedirs(dirpath)
-        shutil.copyfile(TEMP_FILE,PATH(dirpath + "/" + imagename + "." + form))
+        shutil.copyfile(
+            TEMP_FILE,
+            PATH(
+                dirpath +
+                "/" +
+                imagename +
+                "." +
+                form))
 
-    #加载目标文件
+    # 加载目标文件
     def load_image(self, image_path):
         if os.path.isfile(image_path):
             load = Image.open(image_path)
             return load
         else:
-            raise Exception("%s is not exist" %image_path)
+            raise Exception("%s is not exist" % image_path)
 
-    #图片分割
+    # 图片分割
     def split_image(self, img, part_size=(64, 64)):
         w, h = img.size
 
@@ -66,34 +78,54 @@ class Appium_Extend(object):
 
         assert w % pw == h % ph == 0
 
-        return [img.crop((i, j, i+pw, j+ph)).copy() \
+        return [img.crop((i, j, i + pw, j + ph)).copy()
 
-                    for i in range(0, w, pw) \
+                for i in range(0, w, pw)
 
-                    for j in range(0, h, ph)]
+                for j in range(0, h, ph)]
 
-
-    #图片直方图对比
+    # 图片直方图对比
     def hist_similar(self, lh, rh):
-        assert len(lh) == len(rh)
-        return sum(1 - (0 if l == r else float(abs(l - r))/max(l, r)) for l, r in zip(lh, rh))/len(lh)
+        return sum(1 - (0 if l == r else float(abs(l - r)) / max(l, r))
+                   for l, r in zip(lh, rh)) / len(lh)
 
-    #对比图片
+    def avhash(self, im):
+        im = im.resize((8, 8), Image.ANTIALIAS).convert('L')
+        avg = reduce(lambda x, y: x + y, im.getdata()) / 64
+        return reduce(lambda x, y_z: x | (y_z[1] << y_z[0]),
+               enumerate(map(lambda i: 0 if i < avg else 1, im.getdata())),
+               0)
+
+    def hamming(self, h1, h2):
+        h, d = 0, h1 ^ h2
+        while d:
+            h += 1
+            d &= d-1
+        return h
+
+
+    # 对比图片
     def same_as(self, load_image, percent):
         image1 = Image.open(TEMP_FILE)
         image2 = load_image
-        li = image1.resize((256, 256)).convert('RGB')
-        ri = image2.resize((256, 256)).convert('RGB')
-
-        differ = sum(self.hist_similar(l.histogram(), r.histogram()) for l, r in zip(self.split_image(li), self.split_image(ri))) / 16.0
+        #os.startfile(TEMP_FILE)
+        #os.startfile("D:/py/xlbbtest/xlbbtest/loginimage/2.png")
+        im1_hash=self.avhash(image1)
+        im2_hash=self.avhash(image2)
+        h=self.hamming(im1_hash, im2_hash)
+        # li = image1.resize((256, 256)).convert('RGB')
+        # ri = image2.resize((256, 256)).convert('RGB')
+        # differ1 = self.hist_similar(image1.histogram(), image2.histogram())
+        # print(str(differ1))
+        # differ = sum(
+        #     self.hist_similar(
+        #         l.histogram(), r.histogram()) for l, r in zip(
+        #         self.split_image(li), self.split_image(ri))) / 16.0
+        # print(str(differ))
         #differ = sum(1 - (0 if l == r else float(abs(l - r))/max(l, r)) for l, r in zip(lh, rh))/len(lh)
         # differ = math.sqrt(reduce(operator.add,  list(map(lambda a,b: (a-b)**2,  histogram1, histogram2)))/len(histogram1) )
-        if differ >= percent:
+        if h <= percent:
+            print('info:汉明距离：'+str(h))
             return True
         else:
             return False
-
-
-
-
-
--- test_script.py
+++ test_script.py
@@ -1,7 +1,78 @@
 import os
-from image_extend import  Appium_Extend
+from image_extend import Appium_Extend
 from time import sleep, time
 from appium import webdriver
+
+
+def time_decorator(function):  # 时间花费修饰器
+    def wrapper():
+        start_time = time()
+        function()
+        print('time=' + str((time() - start_time)) + 's')
+        print('--------------------------')
+    return wrapper
+
+
+def pre_image_verify_decorator(*args):  # 脚本调用图片验证验证
+    image_path = args[0]
+    secs = args[1]
+
+    def decorator(function):
+        def wrapper():
+            result = False
+            sec = 0
+            while result == False:
+                sleep(1)
+                extend = Appium_Extend(driver)
+                element = driver.find_element_by_class_name(
+                    "android.widget.FrameLayout")
+                load = extend.load_image(image_path)
+                result = extend.get_screenshot_by_element(
+                    element).same_as(load, 10)
+                if result:
+                    print('info:验证通过')
+                else:
+                    if sec <= secs:
+                        print('.')
+                        sec += 1
+                    else:
+                        print('info:验证超时')
+                        break
+            function()
+            print('--------------------------')
+        return wrapper
+    return decorator
+
+
+def after_image_verify_decorator(*args):  # 脚本调用后置图片验证
+    image_path = args[0]
+    secs = args[1]
+
+    def decorator(function):
+        def wrapper():
+            function()
+            result = False
+            sec = 0
+            while not result:
+                sleep(1)
+                extend = Appium_Extend(driver)
+                element = driver.find_element_by_class_name(
+                    "android.widget.FrameLayout")
+                load = extend.load_image(image_path)
+                result = extend.get_screenshot_by_element(
+                    element).same_as(load, 10)
+                if result:
+                    print('info:验证通过')
+                else:
+                    if sec <= secs:
+                        print('.')
+                        sec += 1
+                    else:
+                        print('info:验证超时')
+                        break
+            print('--------------------------')
+        return wrapper
+    return decorator
 
 PATH = lambda p: os.path.abspath(
     os.path.join(os.path.dirname(__file__), p)
@@ -13,35 +84,40 @@ desired_caps['platformVersion'] = '4.4.4'
 desired_caps['deviceName'] = 'xiaobai'
 desired_caps['appPackage'] = 'com.baplay.gd.wp'
 desired_caps['appActivity'] = 'com.baplay.gd.wp.UnityPlayerActivity'
+global driver
 driver = webdriver.Remote('http://172.16.40.20:4723/wd/hub', desired_caps)
-sleep(15)
-driver.tap([(540, 1560), ])
-starttime = time()
-while True:
-    el = driver.find_elements_by_android_uiautomator(
-        'new UiSelector().text("登 入")')
-    sleep(1)
-    if el:
-        break
-speet = time() - starttime
-print('进入时间为：' + str(speet))
-driver.tap([(540, 1620), ])
-sleep(3)
-print('成功进入登录页面')
-print('开始测试免注册登录')
-driver.tap([(458, 693), ])
-sleep(3)
-print('选择稍后再说')
-driver.tap([(170, 1500), ])
-sleep(10)
-print('点击进入游戏')
-extend = Appium_Extend(driver)
-element = driver.find_element_by_class_name("android.view.View")
-load = extend.load_image("D:/py/xlbbtest/xlbbtest/loginimage/1.png")
-result = extend.get_screenshot_by_element(element).same_as(load, 0.8)
-if result:
-    print("相似度验证通过"+str(result))
-    print("点击进入游戏"+str(result))
-    driver.tap([(513, 1768), ])
+
+
+@time_decorator
+@pre_image_verify_decorator('D:/py/xlbbtest/xlbbtest/loginimage/1.png', 10)
+# 打开app到第一屏
+def step1():
+    driver.tap([(540, 1560), ])
+
+
+@time_decorator
+@pre_image_verify_decorator('D:/py/xlbbtest/xlbbtest/loginimage/2.png', 12)
+# 打开app到第一屏
+def step2():
+
     sleep(3)
-    driver.tap([(513, 1768), ])
+    driver.tap([(458, 693), ])
+
+
+@time_decorator
+@after_image_verify_decorator('D:/py/xlbbtest/xlbbtest/loginimage/3.png', 14)
+# 打开app到第一屏
+def step3():
+    sleep(3)
+    print('选择稍后再说')
+    driver.tap([(170, 1500), ])
+    sleep(10)
+    print('点击进入游戏')
+    driver.tap([(540, 1762), ])
+
+
+step1()
+sleep(8)
+driver.tap([(540, 1628), ])
+step2()
+step3()
